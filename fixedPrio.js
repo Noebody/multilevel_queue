f7d1f0c153792108f89f846f187ab3e264d27236
Quan Ky Tuoc <tuoc.qk40123@sinhvien.hoasen.edu.vn>
2023-06-18 07:48:28 -0400

    prettying slice

--- fixedPrio.js
+++ fixedPrio.js
@@ -1,90 +1,77 @@
 // create an algorith for fixed priority scheduling
 
-const PRIORITY_LEVELS = {
-    0: 'LOW',
-    1: 'MEDIUM', 
-    2: 'HIGH' 
-  };
-  
-  // Ready queue for each priority level
-  let readyQueue = {
-    0: [],   // Low priority queue
-    1: [],   // Medium priority queue
-    2: []    // High priority queue 
-  };
-  
-  // Currently executing process 
-  let currentProcess = null; 
-
-  // Process
-  class Process {
-    constructor(pid, priority, cpuTime, ioTime) {
-      this.pid = pid;
-      this.priority = priority;
-      this.cpuTime = cpuTime;
-      this.ioTime = ioTime;
-    }
+// Process
+class Process {
+  constructor(pid, priority, cpuTime, ioTime) {
+    this.pid = pid;
+    this.priority = priority;
+    this.cpuTime = cpuTime;
+    this.ioTime = ioTime;
   }
-  
-  // Function to add a new process to the ready queue
-  function addToReadyQueue(process) {
-    // Add the process to the appropriate ready queue
-    readyQueue[process.priority].push(process);
-  }
-  
-  // CPU scheduler
-  function cpuScheduler() {
-    for (let priority = 2; priority >= 0; priority--) {
-      // Check if there are any processes in the ready queue
-      if (readyQueue[priority].length > 0) {
-        // Get the first process in the queue
-        currentProcess = readyQueue[priority].shift();
-        // Stop checking
-        break;
-      }
-    }
+}
+// Ready queue for each priority level
+let queue = [];
 
-    // If there is a process to execute
-    if (currentProcess) {
-      // Execute the process for one time unit
-      executeProcess(currentProcess);
-      // If the process is not finished
-      if (currentProcess.cpuTime > 0) {
-        // Add the process back to the ready queue
-        addToReadyQueue(currentProcess);
-      }
-      // Reset the current process
-      currentProcess = null;
-      // Call the scheduler again
-      cpuScheduler();
-    }
-    
+// Function to add a new process to the ready queue
+function addToQueue(process) {
+  // If queue is empty, add the process
+  if (queue.length == 0) {
+    queue.push(process);
+    return;
   }
 
-  // time variable
-  let time = 0;
+  // Find the position to insert the process
+  let i = 0;
+  while (i < queue.length && queue[i].priority >= process.priority) {
+    i++;
+  }
+  // Insert the process at the position
+  queue.splice(i, 0, process);
 
-  // Function to execute a process for one time unit
-  function executeProcess(process) {
-    // Decrement the CPU burst of the process
-    process.cpuTime--;
-    // Increment the time
-    time++;
-    // Print the time and process ID
-    console.log(`Time ${time}: Process ${process.pid} is running`);
+  // Print the ready queue
+  console.log("Ready queue: ");
+  for (let i = 0; i < queue.length; i++) {
+    console.log(
+      `Process ${queue[i].pid} Priority ${queue[i].priority} CPU time ${queue[i].cpuTime} I/O time ${queue[i].ioTime}`
+    );
+  }
+}
+
+// Function to execute a process for one time unit
+function executeProcess(process) {
+  // Print the process being executed
+  console.log(`Executing process ${process.pid}`);
+
+  // Decrement the CPU time
+  process.cpuTime--;
+
+  // If CPU time is zero, remove the process from the queue
+  if (process.cpuTime == 0) {
+    console.log(`Process ${process.pid} completed`);
+    queue.shift();
+  }
+}
+
+function processQueue() {
+  // If queue is empty, return
+  if (queue.length == 0) {
+    console.log("No more processes to execute");
+    return;
   }
 
-    
-  // Add some sample processes to the queue  
-  addToReadyQueue(new Process(1, 0, 3, 0)); // LOW priority
-  addToReadyQueue(new Process(2, 1, 6, 3)); // MEDIUM priority
-  addToReadyQueue(new Process(3, 2, 4, 4)); // HIGH priority
-  addToReadyQueue(new Process(4, 0, 5, 5)); // LOW priority
-  addToReadyQueue(new Process(5, 1, 2, 7)); // MEDIUM priority
-  console.log("ready queue:\n",readyQueue);
+  // Execute the process at the front of the queue
+  executeProcess(queue[0]);
 
-  
-  // Call the scheduler
-  cpuScheduler();
-  
+  // Execute the next process in the queue
+  processQueue();
+}
 
+// Add some sample processes to the queue
+addToQueue(new Process(1, 0, 3, 0)); // LOW priority
+addToQueue(new Process(2, 1, 6, 3)); // MEDIUM priority
+addToQueue(new Process(3, 2, 4, 4)); // HIGH priority
+addToQueue(new Process(4, 0, 5, 5)); // LOW priority
+addToQueue(new Process(5, 1, 2, 7)); // MEDIUM priority
+
+// Call the scheduler
+processQueue();
--- timeSlice.js
+++ timeSlice.js
@@ -1,59 +1,123 @@
-// Time slice 
-const TIME_SLICE = 5;  
+// create an algorith for time slicing scheduling
 
-// Ready queue 
-let readyQueue = [];  
-
-// Currently executing process
-let currentProcess = null;  
-
-// Function to add a new process to ready queue
-function addToReadyQueue(process) {
-  readyQueue.push(process); 
+// Process
+class Process {
+  constructor(pid, priority, cpuTime, ioTime) {
+    this.pid = pid;
+    this.priority = priority;
+    this.cpuTime = cpuTime;
+    this.ioTime = ioTime;
+  }
 }
 
-// CPU scheduler
-function cpuScheduler() {
-  // While there are processes in the ready queue or a process is executing
-  while(readyQueue.length > 0 || currentProcess != null) {
-    
-    // If a process was preempted, add it back to ready queue
-    if(currentProcess != null) {
-      readyQueue.push(currentProcess); 
-      currentProcess = null; 
-    }  
-    
-    // Pick the next process from ready queue
-    currentProcess = readyQueue.shift();  
-    
-    // Execute the process for 1 unit time 
-    currentProcess.cpuTime--;  
-    
-    // If process completes, continue 
-    if(currentProcess.cpuTime == 0) continue; 
-    
-    // Preempt the process and add back to ready queue 
-    if(currentProcess.timer == TIME_SLICE) {
-      currentProcess.timer = 0;   // Reset timer
-      addToReadyQueue(currentProcess);
-      currentProcess = null; 
+let highPrioQueue = [];
+let lowPrioQueue = [];
+let midPrioQueue = [];
+
+// Function to add a new process to the ready queue
+function addToQueue(process) {
+  // If process priority is 2, add the process to the high priority queue
+  if (process.priority == 2) {
+    highPrioQueue.push(process);
+  }
+  // If process priority is 1, add the process to the mid priority queue
+  else if (process.priority == 1) {
+    midPrioQueue.push(process);
+  }
+  // If process priority is 0, add the process to the low priority queue
+  else {
+    lowPrioQueue.push(process);
+  }
+
+  // Print the ready queue
+  console.log("Ready queue: ");
+  for (let i = 0; i < highPrioQueue.length; i++) {
+    console.log(
+      `Process ${highPrioQueue[i].pid} Priority ${highPrioQueue[i].priority} CPU time ${highPrioQueue[i].cpuTime} I/O time ${highPrioQueue[i].ioTime}`
+    );
+  }
+  for (let i = 0; i < midPrioQueue.length; i++) {
+    console.log(
+      `Process ${midPrioQueue[i].pid} Priority ${midPrioQueue[i].priority} CPU time ${midPrioQueue[i].cpuTime} I/O time ${midPrioQueue[i].ioTime}`
+    );
+  }
+  for (let i = 0; i < lowPrioQueue.length; i++) {
+    console.log(
+      `Process ${lowPrioQueue[i].pid} Priority ${lowPrioQueue[i].priority} CPU time ${lowPrioQueue[i].cpuTime} I/O time ${lowPrioQueue[i].ioTime}`
+    );
+  }
+}
+
+// Function to execute a process for one time unit
+function executeProcess(process) {
+  // Print the process being executed
+  console.log(`Executing process ${process.pid}`);
+
+  // Decrement the CPU time based on priority
+  if (process.priority == 2) {
+    process.cpuTime -= 1; //  4/7 as fraction
+  } else if (process.priority == 1) {
+    process.cpuTime -= 0.5; //  2/7 as fraction
+  } else {
+    process.cpuTime -= 0.25; //  1/7 as fraction
+  }
+
+  // If CPU time is zero, remove the process from the queue
+  if (process.cpuTime == 0) {
+    console.log(`Process ${process.pid} completed`);
+    if (process.priority == 2) {
+      highPrioQueue.shift();
+    } else if (process.priority == 1) {
+      midPrioQueue.shift();
     } else {
-      currentProcess.timer++;  // Increment timer
+      lowPrioQueue.shift();
     }
   }
 }
 
-// Process
-function Process(id, cpuTime) {
-  this.id = id; 
-  this.cpuTime = cpuTime;     // Remaining CPU time
-  this.timer = 0;            // Timer for time slice
+function processQueue() {
+  while (
+    highPrioQueue.length > 0 ||
+    midPrioQueue.length > 0 ||
+    lowPrioQueue.length > 0
+  ) {
+    // If high priority queue is not empty, execute the first process
+    if (highPrioQueue.length > 0) {
+      executeProcess(highPrioQueue[0]);
+    } else {
+      console.log("No high priority process");
+    }
+
+    // If mid priority queue is not empty, execute the first process
+    if (midPrioQueue.length > 0) {
+      executeProcess(midPrioQueue[0]);
+    } else {
+      console.log("No mid priority process");
+    }
+
+    // If low priority queue is not empty, execute the first process
+    if (lowPrioQueue.length > 0) {
+      executeProcess(lowPrioQueue[0]);
+    } else {
+      console.log("No low priority process");
+    }
+  }
 }
 
-// Add some sample processes to the queue
-addToReadyQueue(new Process(1, 10));
-addToReadyQueue(new Process(2, 5)); 
-addToReadyQueue(new Process(3, 20));
+// Create processes
+let p1 = new Process(1, 2, 7, 2);
+let p2 = new Process(2, 1, 4, 1);
+let p3 = new Process(3, 0, 1, 4);
+let p4 = new Process(4, 1, 4, 4);
+let p5 = new Process(5, 2, 5, 1);
 
-// Call the scheduler
-cpuScheduler();
+// Add processes to the ready queue
+addToQueue(p1);
+addToQueue(p2);
+addToQueue(p3);
+addToQueue(p4);
+addToQueue(p5);
+
+// Execute processes
+processQueue();
+processQueue();
