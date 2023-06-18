808b6d4df48d86deaae42ce7086966630f92df98
Nguyen Phung Hung <temredalert3@gmail.com>
2023-06-18 12:57:24 -0400

    Optimized code

--- cpu/src/app/page.js
+++ cpu/src/app/page.js
@@ -1,7 +1,6 @@
 "use client";
 
-import Image from "next/image";
-import { useState, useEffect, useMemo } from "react";
+import { useState, useEffect } from "react";
 
 export default function Home() {
   const [queue, setQueue] = useState([]);
@@ -15,11 +14,7 @@ export default function Home() {
     };
   }
 
-  const sortedQueue = useMemo(() => {
-    return queue.sort((a, b) => a.priority - b.priority);
-  }, [queue]);
-
-  console.log(sortedQueue);
+  console.log(queue);
 
   // Function to execute a process for one time unit
   function executeProcess(process) {
@@ -27,38 +22,54 @@ export default function Home() {
     console.log(`Executing process ${process.pid}`);
 
     // Decrement the CPU time
-    process.cpuTime--;
+    setQueue((prev) => {
+      return prev.map((item) => {
+        if (item.pid == process.pid) {
+          return {
+            ...item,
+            cpuTime: item.cpuTime - 1,
+          };
+        }
+        return item;
+      });
+    });
 
     // If CPU time is zero, remove the process from the queue
     if (process.cpuTime == 0) {
       console.log(`Process ${process.pid} completed`);
-      queue.shift();
     }
   }
 
   function processQueue() {
     // If queue is empty, return
-    if (queue.length == 0) {
-      console.log("No more processes to execute");
-      return;
-    }
+    // if (queue.length == 0) {
+    //   console.log("No more processes to execute");
+    //   return;
+    // }
 
     // Execute the process at the front of the process
-    executeProcess(process[0]);
+    queue.forEach((process) => {
+      executeProcess(process);
+    });
 
     // Execute the next process in the process
-    processQueue();
   }
 
   useEffect(() => {
-    setQueue([
-      createProcess(1, 0, 3, 0),
-      createProcess(2, 1, 6, 3),
-      createProcess(3, 2, 4, 4),
-      createProcess(4, 0, 5, 5),
-      createProcess(5, 1, 2, 7),
-    ]);
+    setQueue(
+      [
+        createProcess(1, 0, 3, 0),
+        createProcess(2, 1, 6, 3),
+        createProcess(3, 2, 4, 4),
+        createProcess(4, 0, 5, 5),
+        createProcess(5, 1, 2, 7),
+      ].sort((a, b) => a.priority - b.priority)
+    );
   }, []);
+
+  useEffect(() => {
+    processQueue();
+  }, [queue.length]);
 
   return (
     <main className="">
@@ -149,137 +160,8 @@ export default function Home() {
             </tr>
           </tbody>
         </table>
-
-        <table class="table-auto">
-          <thead>
-            <tr>
-              <th class="px-4 py-2">Process</th>
-              <th class="px-4 py-2">Arrival Time</th>
-              <th class="px-4 py-2">Burst Time</th>
-              <th class="px-4 py-2">Priority</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td class="border px-4 py-2">P1</td>
-              <td class="border px-4 py-2">0</td>
-              <td class="border px-4 py-2">8</td>
-              <td class="border px-4 py-2">2</td>
-            </tr>
-            <tr class="bg-gray-100">
-              <td class="border px-4 py-2">P2</td>
-              <td class="border px-4 py-2">1</td>
-              <td class="border px-4 py-2">4</td>
-              <td class="border px-4 py-2">1</td>
-            </tr>
-            <tr>
-              <td class="border px-4 py-2">P3</td>
-              <td class="border px-4 py-2">2</td>
-              <td class="border px-4 py-2">9</td>
-              <td class="border px-4 py-2">3</td>
-            </tr>
-            <tr class="bg-gray-100">
-              <td class="border px-4 py-2">P4</td>
-              <td class="border px-4 py-2">3</td>
-              <td class="border px-4 py-2">5</td>
-              <td class="border px-4 py-2">2</td>
-            </tr>
-            <tr>
-              <td class="border px-4 py-2">P5</td>
-              <td class="border px-4 py-2">4</td>
-              <td class="border px-4 py-2">2</td>
-              <td class="border px-4 py-2">3</td>
-            </tr>
-          </tbody>
-        </table>
-
-        <table class="table-auto">
-          <thead>
-            <tr>
-              <th class="px-4 py-2">Process</th>
-              <th class="px-4 py-2">Arrival Time</th>
-              <th class="px-4 py-2">Burst Time</th>
-              <th class="px-4 py-2">Priority</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td class="border px-4 py-2">P1</td>
-              <td class="border px-4 py-2">0</td>
-              <td class="border px-4 py-2">8</td>
-              <td class="border px-4 py-2">2</td>
-            </tr>
-            <tr class="bg-gray-100">
-              <td class="border px-4 py-2">P2</td>
-              <td class="border px-4 py-2">1</td>
-              <td class="border px-4 py-2">4</td>
-              <td class="border px-4 py-2">1</td>
-            </tr>
-            <tr>
-              <td class="border px-4 py-2">P3</td>
-              <td class="border px-4 py-2">2</td>
-              <td class="border px-4 py-2">9</td>
-              <td class="border px-4 py-2">3</td>
-            </tr>
-            <tr class="bg-gray-100">
-              <td class="border px-4 py-2">P4</td>
-              <td class="border px-4 py-2">3</td>
-              <td class="border px-4 py-2">5</td>
-              <td class="border px-4 py-2">2</td>
-            </tr>
-            <tr>
-              <td class="border px-4 py-2">P5</td>
-              <td class="border px-4 py-2">4</td>
-              <td class="border px-4 py-2">2</td>
-              <td class="border px-4 py-2">3</td>
-            </tr>
-          </tbody>
-        </table>
-
-        <table class="table-auto" id="table-4">
-          <thead>
-            <tr>
-              <th class="px-4 py-2">Process</th>
-              <th class="px-4 py-2">Arrival Time</th>
-              <th class="px-4 py-2">Burst Time</th>
-              <th class="px-4 py-2">Priority</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td class="border px-4 py-2">P1</td>
-              <td class="border px-4 py-2">0</td>
-              <td class="border px-4 py-2">8</td>
-              <td class="border px-4 py-2">2</td>
-            </tr>
-            <tr class="bg-gray-100">
-              <td class="border px-4 py-2">P2</td>
-              <td class="border px-4 py-2">1</td>
-              <td class="border px-4 py-2">4</td>
-              <td class="border px-4 py-2">1</td>
-            </tr>
-            <tr>
-              <td class="border px-4 py-2">P3</td>
-              <td class="border px-4 py-2">2</td>
-              <td class="border px-4 py-2">9</td>
-              <td class="border px-4 py-2">3</td>
-            </tr>
-            <tr class="bg-gray-100">
-              <td class="border px-4 py-2">P4</td>
-              <td class="border px-4 py-2">3</td>
-              <td class="border px-4 py-2">5</td>
-              <td class="border px-4 py-2">2</td>
-            </tr>
-            <tr>
-              <td class="border px-4 py-2">P5</td>
-              <td class="border px-4 py-2">4</td>
-              <td class="border px-4 py-2">2</td>
-              <td class="border px-4 py-2">3</td>
-            </tr>
-          </tbody>
-        </table>
       </div>
-      <div></div>
+      <div>{}</div>
     </main>
   );
 }
